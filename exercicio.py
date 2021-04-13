97250afc7c617f929444f4455c74018618017fc4
Francisco Bustamante <chicolucio@gmail.com>
2021-04-13 14:13:59 -0300

    Primeira reuniao grafico gases ideais

--- exercicio.py
+++ exercicio.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def replace_comma_to_dot(string):
@@ -6,8 +7,14 @@ def replace_comma_to_dot(string):
 
 
 dados = np.genfromtxt((replace_comma_to_dot(text) for text in open('exercicio_aula_1.csv')),
-                      delimiter=';', dtype=(float, float), names=True)
+                      delimiter=';', dtype=(float, float), names=['P_bar', 'd_gL'], skip_header=1)
 
+pressao_sobre_densidade = dados['P_bar'] / dados['d_gL']
 
-print(dados)
-# print(dados['P_bar'])
+fig, ax = plt.subplots(figsize=(8, 6), nrows=1, ncols=1)
+
+ax.scatter(dados['d_gL'], pressao_sobre_densidade)
+
+print(dados.dtype.names)
+
+plt.show()
